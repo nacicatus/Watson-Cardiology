af74d1144d7ad05ce4625ae27bae4d7c34e8f9bc
SaurabhSikka <drsaurabhsikka@gmail.com>
2016-10-21 15:22:21 +0200

    Initial Commit

---
+++ Watson Cardiology/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watson Cardiology
+//
+//  Created by Saurabh Sikka on 21/10/16.
+//  Copyright © 2016 Saurabh Sikka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
